769b0d08d7f650fe6764585bf68efeea45579b62
B-S-B-Rabbit <mystery.knight113@gmail.com>
2023-05-11 20:26:19 +0300

    add Dockerfile4, requirements

--- coursepaper/settings.py
+++ coursepaper/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = 'django-insecure-1!#j##ltf(o^xagegw+dxe70(@l#(a$+2%reg#d*s4w=nyyk^i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shsai-mysteryknight113.b4a.run']
+
 
 # Application definition
 
